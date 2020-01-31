342905bdd36c28316f697179a3eb42f592c25a02
JJC <jjc@nlplab.cc>
2020-01-31 18:59:57 +0800

    feat: add trigram edit suggest api

---
+++ trigram_suggest_rest_api.py
@@ -0,0 +1,34 @@
+from starlette.responses import UJSONResponse
+from fastapi import FastAPI
+
+from linggle_api import LinggleAPI
+import enchant
+
+app = FastAPI()
+linggle = LinggleAPI()
+spell = enchant.Dict('en')
+
+
+def gen_replace_query(word):
+    if spell.check(word):
+        # check forms
+        return '_'
+    else:
+        return '/'.join(spell.suggest(word))
+
+
+@app.get("/{ngramstr}", response_class=UJSONResponse)
+def check_ngram(ngramstr: str, err_type: str = None):
+    ngram = ngramstr.split()
+    if err_type == 'replace':
+        ngram[1] = gen_replace_query(ngram[1])
+        res = linggle.query(' '.join(ngram))._asdict()
+    elif err_type == 'delete':
+        ngram[1] = '?' + ngram[1]
+        res = linggle.query(' '.join(ngram))._asdict()
+    elif err_type == 'insert':
+        cmd = ' '.join((ngram[0], '_', ngram[1]))
+        res = linggle.query(cmd)._asdict()
+    else:
+        res = linggle.query(ngramstr)._asdict()
+    return res
