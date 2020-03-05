9330013192571af065ded200b04e74ce20037973
JJC <jjc@nlplab.cc>
2020-03-05 14:56:15 +0800

    feat: update linggle API to the new format

--- linggle_api.py
+++ linggle_api.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from collections import namedtuple
 import requests
 import urllib
 
 NGRAM_API_URI = "https://{0}.linggle.com/query/"
 EXP_API_URI = "https://{0}.linggle.com/example/"
-
-
-NgramResult = namedtuple("NgramResult", ["query", "ngrams", "total"])
 
 
 class LinggleAPI(dict):
@@ -21,31 +17,33 @@ class LinggleAPI(dict):
         return self.query(query)
 
     def query(self, query):
-        query = query.replace('/', '@')
-        query = urllib.parse.quote(query, safe='')
-        req = requests.get(self.ngram_api + query)
+        req = requests.get(self.ngram_api + urllib.parse.quote(query, safe=''))
         if req.status_code == 200:
-            return NgramResult(**req.json())
+            r = req.json()
+            return r['ngrams']
+        else:
+            # TODO: handle when status code is not 200
+            pass
 
     def get_example(self, ngram_str):
         req = requests.post(self.example_api, json={'ngram': ngram_str})
         if req.status_code == 200:
             result = req.json()
-            return result.get("examples", [])
+            return result["examples"]
+        else:
+            # TODO: handle when status code is not 200
+            pass
 
 
 if __name__ == "__main__":
     linggle = LinggleAPI()
-
-    result = linggle["adj. beach"]
-    print("Total:", result.total)
-    for ngram, count in result.ngrams[:3]:
+    for ngram, count in linggle["adj. beach"][:3]:
         print(ngram, count)
     for sent in linggle.get_example("sandy beach")[:3]:
         print(sent)
 
     linggle = LinggleAPI('zh')
-    for ngram, count in linggle.query("提出 n.").ngrams[:3]:
+    for ngram, count in linggle.query("提出 n.")[:3]:
         print(ngram, count)
     for sent in linggle.get_example("提出 報告")[:3]:
         print(sent)
