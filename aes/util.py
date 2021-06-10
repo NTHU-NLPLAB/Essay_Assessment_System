fe711f8bedb7b3da15030723b9319f7fa4f7c19c
JJC <jjc@nlplab.cc>
2021-06-10 15:17:56 +0800

    refactor: simplifying input preprocess, output projection, etc.
    * rewrite OOV preprocess using list comprehension
    * rewrite score normalization and mapping to CEFR levels

--- aes/util.py
+++ aes/util.py
@@ -9,6 +9,7 @@ import numpy as np
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 AES_MODEL_PATH = os.getenv('AES_MODEL_PATH') or os.path.join(APP_DIR, 'model/best_model.h5')
 VOCAB_FILE_PATH = os.getenv('VOCAB_FILE_PATH') or os.path.join(APP_DIR, 'model/efvocab.pkl')
+CEFR_LEVELS = ('A1', 'A2', 'B1', 'B2', 'C1', 'C2')
 
 aes_model = load_model(AES_MODEL_PATH)
 # test
@@ -19,38 +20,9 @@ with open(VOCAB_FILE_PATH, 'rb') as vocab_file:
     vocab = pk.load(vocab_file)
 
 
-def read_sentence(line, vocab):
-    data_x = []
-    for word in line.lower().split(' '):
-        if word in vocab:
-            data_x.append(vocab[word])
-        else:
-            data_x.append(vocab['<unk>'])
-    return data_x
-
-
-def convert_to_dataset_friendly_scores(scores_array, ranges=(0, 5)):
-    low, high = ranges[0], ranges[1]
-    scores_array = scores_array * (high - low) + low
-    return scores_array
-
-
 def predict_cerf(text):
-    text = text.strip()
-    test_sentence = read_sentence(text, vocab)
-    test_sentence = sequence.pad_sequences([test_sentence], maxlen=350)
-    cerf_level = round(convert_to_dataset_friendly_scores(model.predict(test_sentence).squeeze()))
-
-    if cerf_level == 0.0:
-        cerf_level = 'A1'
-    elif cerf_level == 1.0:
-        cerf_level = 'A2'
-    elif cerf_level == 2.0:
-        cerf_level = 'B1'
-    elif cerf_level == 3.0:
-        cerf_level = 'B2'
-    elif cerf_level == 4.0:
-        cerf_level = 'C1'
-    elif cerf_level == 5.0:
-        cerf_level = 'C2'
-    return cerf_level, ''
+    # replace OOV tokens to '<unk>' tokens
+    tokens = [vocab[word] if word in vocab else vocab['<unk>'] for word in text.strip().lower().split()]
+    test_sentence = sequence.pad_sequences([tokens], maxlen=350)
+    score = aes_model.predict(test_sentence).squeeze()
+    return CEFR_LEVELS[int(round(score * 5))]
